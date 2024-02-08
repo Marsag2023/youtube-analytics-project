d12c50def807d663b72d04263c94d7f0a7322ede
Marsag2023 <m.aygistov@gmail.com>
2024-02-08 09:57:10 +0300

    Add program cod

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,25 @@
+import os
+from googleapiclient.discovery import build
+from pprint import pprint
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """
+    Класс для ютуб-канала
+    """
+    api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """
+        Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API.
+        """
+        self.channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        """
+        Выводит в консоль информацию о канале.
+        """
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        pprint(channel)
